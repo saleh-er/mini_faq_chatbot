facaa889af73727891f550b025a609ee59a17e86
saleh <saleherragragui02@gmail.com>
2026-02-10 00:11:22 +0100

    update

--- chatbot_console.py
+++ chatbot_console.py
@@ -3,11 +3,11 @@ from chatbot.core import ChatBot
 bot = ChatBot()
 
 if __name__ == "__main__":
-    print("Bienvenue dans le Mini Chatbot FAQ ! (tapez 'quit' pour quitter)")
+    print("Welcome to Mini FAQ Chatbot! (type 'quit' to exit)")
     while True:
-        q = input("Vous : ")
+        q = input("You: ")
         if q.lower() == 'quit':
             break
         questions = q.split(";")
         for question in questions:
-            print("Bot :", bot.get_answer(question.strip()))
+            print("Bot:", bot.get_answer(question.strip()))
